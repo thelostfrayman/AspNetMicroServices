82c56d102d7c812331bc5616b1977c3343e77e1d
frayman <frayman@SHADOW>
2021-08-15 17:13:52 -0500

    added business layer ProductRespository

---
+++ src/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetProduct(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        Task<IEnumerable<Product>> GetProductByCategory(string CategoryName);
+
+        Task CreateProcuduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+
+    }
+}
